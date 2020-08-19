d68e38de0180cbb536cda6f3cf069aae3ca6d062
vnguyen38 <vnguyen38@huskers.unl.edu>
2020-08-19 14:25:52 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Vivian Nguyen
+ *         vnguyen38@huskers.unl.edu
+ * Date: 2020/08/14
+ *
+ * A simple name and major program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Vivian Nguyen!\n");
+  printf("Undecided\n");
+  
+  return 0;
+}
